9e4a755347c7fee85668ab5eb68ddb0e35ea2f95
Alisher Ubaydullayev <159762352+Alisher-Ubaydullayev@users.noreply.github.com>
2024-04-23 06:04:01 +0500

    if-else va hatolar bilan ishlash
    if else va hatolar bilan ishlsh darsida yozilgan kodlar

---
+++ lesson_1.py
@@ -0,0 +1,108 @@
+#kun = input("Bugun kun nima? ")
+#if kun.lower()=='shanba'or kun.lower()=='yakshanba':
+#    print("bugun dam olsh kuni")
+#else:
+#    print("bugun ish kuni")
+
+#kun = input("bugun kun nima? ")
+#havo = int(input("havo harorati necha gradus? "))
+#
+#if kun.lower()=='shanba' or kun.lower()=='yakshanba' and havo<= 30:
+#    print('chomilishga boramiz')
+#else:
+#    print('bugun uyda dam olamiz')
+
+#narx = 15000
+#choy = 0
+#salat = 1
+#if choy and salat:
+#   narx = narx+ 5000
+#elif choy or salat:
+#    narx = narx+10000
+
+#print(f"jami {narx} so'm bo'ldi")
+
+#sant = {'atvot':'20 lik' ,'troynik':"25lik", 'ushka':"25/15tashqi", 'mufta':"32lik"}
+
+#print(sant['mufta'])
+#print(sant['atvot'])
+
+#ishchi = {
+#    'ism':"ali ubaydullayev",
+#    't_yil':1997,
+#    'yosh': 27
+#}
+#print(f"{hodim ['ism'].t}")
+
+#son = float(input("Juft son kiriting: "))
+#if son%2 !=0:
+#    print("Bu son juft emas.")
+#else:
+#    print("Rahmat!")
+
+#yosh = int(input("Yoshingiz nechida?"))
+
+#if yosh<=4 or yosh>=60:
+#    narh = 0
+#elif yosh < 18:
+#    narh = 10000
+#else:
+#    narh = 20000
+#    print(f"Chipta {narh} so'm")
+
+#x = float(input("Birinchi sonni kiriting: "))
+#y = float(input("Ikkinchi sonni kiriting: "))
+#if x==y:
+#    print(f"{x}={y}")
+#elif x<y:
+#    print(f"{x}<{y}")
+#else:
+#    print(f"{x}>{y}")
+
+#mahsulotlar = ['un', "yog'", "sovun", 'tuxum', 'piyoz','kartoshka', 'olma', 'banan', 'uzum', 'qovun']
+#savat=[]
+#
+#for n in range(5):
+#    savat.append(input(f"Savatga {n+1}-mahsulotni qo'shing: "))
+#
+#if savat:
+#    for mahsulot in savat:
+#        if mahsulot in mahsulotlar:
+#            print(f"Do'konimizda {mahsulot} bor")
+#        else:
+#            print(f"Do'konimizda {mahsulot} yo'q")
+#else:
+#    print("Savatingiz bo'sh")
+
+#mahsulotlar = ['un', "yog'", "sovun", 'tuxum', 'piyoz',
+#               'kartoshka', 'olma', 'banan', 'uzum', 'qovun']
+#
+#
+#savat = []
+#for n in range(5):
+#    savat.append(input(f"Savatga {n+1}-mahsulotni qo'shing: "))
+#
+#bor_mahsulotlar = []
+#mavjud_emas = []
+#for mahsulot in savat:
+#    if mahsulot in mahsulotlar:
+#        bor_mahsulotlar.append(mahsulot)
+#    else:
+#        mavjud_emas.append(mahsulot)
+#
+#if mavjud_emas:
+#  print("Do'konimizda quyidagi mahsulotlar yo'q:")
+#for mahsulot in mavjud_emas:
+#  print(mahsulot)
+#else:
+#  print("Siz so'ragan barcha mahsulotlar do'konimizda bor")
+
+#users = ['alisher1983','aziza','yasina' 'umar']
+
+#login = input("Yangi login tanlang:" )
+
+#if login in users:
+#    print('Login band, yangi login tanalng!')
+#else:
+#    print("Xush kelibsiz!")
+
