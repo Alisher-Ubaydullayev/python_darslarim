efc32f6b8b581012dfb6909d3711e26e19733ab6
Alisher Ubaydullayev <159762352+Alisher-Ubaydullayev@users.noreply.github.com>
2024-05-21 12:37:20 +0500

    new

--- lesson_1.py
+++ lesson_1.py
@@ -40,6 +40,9 @@
 #else:
 #    print("Rahmat!")
 
+
+
+
 #yosh = int(input("Yoshingiz nechida?"))
 
 #if yosh<=4 or yosh>=60:
@@ -106,3 +109,93 @@
 #else:
 #    print("Xush kelibsiz!")
 
+
+#import requests
+#from pprint import pprint as print
+
+
+#API_KEY = '8954f20edb4ccce176fa409b'
+
+#currensy = 'USD'
+#url = f'https://v6.exchangerate-api.com/v6/{API_KEY}/pair/{currensy}/UZS'
+
+#r = requests.get(url)
+#print(r.status_code)
+#res = r.json
+#kurs = r.json()['conversion_rate']
+#butun = int(kurs)
+#print(f"Bugungi kurs: {butun} so'm")
+
+
+#oyat = 2
+#sura = 8
+#tafsir = 'uzb-muhammadsodikmu'
+
+ 
+#url_sura = f"https://cdn.jsdelivr.net/gh/fawazahmed0/quran-api@1/editions/{tafsir}/{sura}.json"
+#url_oyat = f"https://cdn.jsdelivr.net/gh/fawazahmed0/quran-api@1/editions/{tafsir}/{sura}/{oyat}.json"
+
+#r = requests.get(url_oyat)
+#print(r.status_code)
+#res = r.json()
+#print(res)
+
+
+#from pprint import pprint as print
+
+
+#app_id = "92cc5baf"
+#app_key = "8ed5d225ffe547a3c1ac5cbfdd088d5a"
+#language = "en-gb"
+
+
+#word_id = "apple"
+#url = f"https://od-api-sandbox.oxforddictionaries.com:443/api/v2/entries/{language}/{word_id.lower()}"
+
+#r = requests.get(url, headers={"app_id": app_id, "app_key": app_key})
+
+#print(r.status_code)
+#res = r.json()
+#print(r)
+
+#talaba = {
+#    'ism': 'alijon',
+#    'familia': 'shmamsiyev',
+#    'yosh':  22,
+#    'fakultitet': 'matematika',
+#    'kurs':  4
+#}
+#print(f"bu lug'atning uzunligi {len(talaba)} ta")
+
+#for kalit, qiymat in talaba.items():
+#    print(f"kalit: {kalit}")
+#    print(f"qiymat: {qiymat}")
+
+#car ={
+
+#}
+
+#a = str(input("mashinangiz markasini yozing: "))
+#b = str(input("mashina rangini kiriting: "))
+#c = int(input('mashina narxini yozing: '))
+
+#car['marka']= a
+#car['rang']= b
+#car['narx']= c
+
+#for k, v in car.items():
+#    print(f'Mashinaning {k}si {v}')
+
+
+#telefonlar = {
+#    'olim': 'galaxys9',
+#    'sardor':'iphone X',
+#    'aliy': 's 24 ultra',
+#    'sherzod': 'nokia 3310'
+#}
+
+#for k, q in telefonlar.items():
+#    print(f'{k.title()} ning telefoni {q}')
+maxsulotlar = {
+    'anor': '20000'
+}
